085bbb75035ab955dbd30611c86d210a3a0b39d2
andrewmatheus <andrewmatheus@gmail.com>
2024-04-15 00:18:02 -0300

    Iniciando projeto

---
+++ src/main/java/com/andrewmatheus/labpcp/LabPcpApplication.java
@@ -0,0 +1,13 @@
+package com.andrewmatheus.labpcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPcpApplication.class, args);
+    }
+
+}
